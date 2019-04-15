ebc9c038676326a98ecca11b9f13b907d41ee914
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2019-04-15 21:28:45 +0530

    Update initial.java

--- initial.java
+++ initial.java
@@ -1,14 +1,54 @@
 package wumpus;
 
-//import javax.swing.JFrame;
-//import javax.swing.JButton;
-//import javax.swing.JPanel;
 import javax.swing.*;
-//import java.awt.GridLayout;
+import java.awt.GridLayout;
+import java.awt.event.*;  
+
 
 public class Own {
+	int index = 12;
+	int num;
+	int score = 1000;
+	String Wumpus = "W";
+	String Smell = "s";
+	
+	String Gold = "G";
+	String Glitter = "g";
+	
+	String Pit = "P";
+	String Breeze = "b";
+	
+	int pitloc = 2;
+	int wumploc = 4;
+	int goldloc = 9;
     public Own(){ 
+    	
+        JButton buttons[] = new JButton[30];
         JFrame frame=new JFrame();
+    	JPanel frame2 = new JPanel();
+    	
+    	frame2.setLayout(new GridLayout(4, 4));
+        
+        for (int i = 0; i < 16; i++) {
+            buttons[i] = new JButton("?");
+            frame2.add(buttons[i]);
+            //buttons[i].setEnabled(false);
+        }
+        
+        buttons[pitloc].setText(Pit);
+        buttons[wumploc].setText(Wumpus);
+        buttons[goldloc].setText(Gold);
+
+        
+        setpos(pitloc,Breeze);
+        setpos(wumploc,Smell);
+        setpos(goldloc,Glitter);
+        
+        
+        buttons[index].setText("^");
+        frame2.setVisible(true);
+        frame2.setBounds(250,180,280,280);    
+        frame.add(frame2);
         
         JButton forward = new JButton("Forward");
         forward.setBounds(30,10,90,60);
@@ -22,19 +62,152 @@ public class Own {
         JButton shoot= new JButton("Shoot");
         shoot.setBounds(30,220,90,60);
         
+        JTextField tf = new JTextField("Current score will be displayed here");
+        tf.setBounds(30,290,180,60);
+        
+        
+        forward.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+            		String s1 = buttons[index].getText();
+            		String s2 = "-";
+            		num = index;
+            		score-=1;
+            		if(s1 == "^")
+            		{
+            			if(index<=3)
+            			{
+            				index+=4;
+            				score+=1;
+            			}
+            			index-=4;
+            		}
+            		if(s1 == ">")
+            		{
+            			if(index%4 == 3)
+            			{
+            				index-=1;
+            				score+=1;
+            			}
+            			index+=1;    
+            		}
+            		if(s1 == "<")
+            		{
+            			if(index%4 == 0)
+            			{
+            				index+=1;
+            				score+=1;
+            			}
+            			index-=1;            	
+            		}
+            		if(s1 == "v")
+            		{
+            			if(index >11)
+            			{
+            				index-=4;
+            				score+=1;
+            			}
+            			index+=4; 
+            		}
+            		buttons[num].setText(s2);
+            		buttons[index].setText(s1); 
+            		tf.setText(String.valueOf(score));
+            }  
+            }); 
+
+        left.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+            		String s1 = buttons[index].getText();
+            		String s2 = "-";
+            		if(s1 == "^")
+            		{
+            			s1 = "<";
+            		}
+            		else if(s1 == ">")
+            		{
+            			s1 = "^";
+            		}
+            		else if(s1 == "<")
+            		{
+            			s1 = "v";
+            		}
+            		else if(s1 == "v")
+            		{
+            			s1 = ">";
+            		}
+                    buttons[index].setText(s1);       
+            }  
+            }); 
+        
+        right.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+            		String s1 = buttons[index].getText();
+            		String s2 = "-";
+            		if(s1 == "^")
+            		{
+            			s1 = ">";
+            		}
+            		else if(s1 == ">")
+            		{
+            			s1 = "v";
+            		}
+            		else if(s1 == "<")
+            		{
+            			s1 = "^";
+            		}
+            		else if(s1 == "v")
+            		{
+            			s1 = "<";
+            		}
+                    buttons[index].setText(s1);       
+            }  
+            }); 
+        
+        shoot.addActionListener(new ActionListener(){  
+            public void actionPerformed(ActionEvent e){  
+            		String s1 = buttons[index].getText();
+            		//String s2 = "-";
+            		score-=10;
+            		if(s1 == "^")
+            		{
+            			s1 = ">";
+            		}
+            		else if(s1 == ">")
+            		{
+            			s1 = "v";
+            		}
+            		else if(s1 == "<")
+            		{
+            			s1 = "^";
+            		}
+            		else if(s1 == "v")
+            		{
+            			s1 = "<";
+            		}
+                    buttons[index].setText(s1);   
+            		tf.setText(String.valueOf(score));
+            }  
+            }); 
+        
         frame.add(forward);
         frame.add(left);
         frame.add(right);
         frame.add(shoot);
+        frame.add(tf);
         
         JPanel grid = new JPanel();
         
         
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-        frame.setSize(1000,1000);
+        frame.setSize(800,800);
         frame.setLayout(null);  
         frame.setVisible(true);
 }
+    
+    void setpos(int i,String s)
+    {
+    	
+    }
+    
 public static void main(String[] args) {
         Own t1 = new Own();//makes new ButtonGrid with 2 parameters
 	}
